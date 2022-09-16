cb37db81887bb4177d7633f08d59634cd11685dc
Andrew Cornejo <atcornejo3@gmail.com>
2022-09-16 11:21:43 -0700

    adding more logic, changed .html to index naming convention, various other fixes

--- app.js
+++ app.js
@@ -2,7 +2,18 @@
 const environmentHeader = document.getElementById('environment-header');
 const territoryHeader = document.getElementById('territory-header');
 const cityHeader = document.getElementById('city-header');
+const championHeader = document.getElementById('champion-header');
+const silverwingHeader = document.getElementById('silverwing-header');
+
 const environmentWorld = document.getElementById('environment-world');
+const environmentTerritory = document.getElementById('environment-territory');
+const environmentCity = document.getElementById('environment-city');
+const environmentChampion = document.getElementById('environment-chmapion');
+const environmentSilverwing = document.getElementById('environment-silverwing');
+
+
+
+
 
 const worldSelect = document.getElementById('world-select'); 
 const territorySelect = document.getElementById('territory-select');
@@ -21,11 +32,11 @@ const silverwingImage = document.getElementById('silverwing-image');
 
 
 let environment = {
-        world: 'The Void',
-        territory: 'The Void',
-        city: 'The Void',
-        champion: 'Garen',
-        silverwing: 'Silverwing',
+        world: '',
+        territory: '',
+        city: '',
+        champion: '',
+        silverwing: '',
 };
 
 
@@ -37,44 +48,47 @@ displayEnvironment();
 
 });
 
-//territorySelect.addEventListener('change', () => {
-    //environment.territory = territorySelect.value;
-   // displayTerritory();
+territorySelect.addEventListener('change', () => {
+    environment.territory = territorySelect.value;
+    displayEnvironment();
     
     
-    //});
+    });
 
-//citySelect.addEventListener('change', () => {
-        //environment.city = citySelect.value;
-       // displayCity();
+citySelect.addEventListener('change', () => {
+        environment.city = citySelect.value;
+        displayEnvironment();
         
         
-       // });
+        });
        
- //championSelect.addEventListener('change', () => {
-            //environment.champion = championSelect.value;
-           // displayChampion();
+ championSelect.addEventListener('change', () => {
+            environment.champion = championSelect.value;
+            displayEnvironment();
             
             
-           // });
-           // silverwingSelect.addEventListener('change', () => {
-                //environment.silverwing = silverwingSelect.value;
-               // displaySilverwing();
+            });
+            silverwingSelect.addEventListener('change', () => {
+                environment.silverwing = silverwingSelect.value;
+                displayEnvironment();
                 
                 
-                //}); 
+                }); 
 
 //
 function displayEnvironment() {
-
-
-
     environmentHeader.textContent = environment.world;
- 
+    environmentWorld.src = 'assets/' + environment.world + '.png';
+
+    environmentTerrirtory.src = 'assets/' + environment.territory + '.png';
+
+    environmentCity.src = 'assets/' + environment.city + '.png';
+
+    environmentWorld.src = 'assets/' + environment.world + '.png';
 
     environmentWorld.src = 'assets/' + environment.world + '.png';
 
 }
 //const addButton = document.getElementById('add-button');
-displayEnvironment;
+displayEnvironment();
 ;
