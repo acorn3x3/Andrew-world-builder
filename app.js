5e2d0b764af6138792d0e4c074c1882c21a84bf9
Andrew Cornejo <atcornejo3@gmail.com>
2022-09-16 14:09:57 -0700

    fixed some logic and typo errors

--- app.js
+++ app.js
@@ -1,94 +1,71 @@
+const worldHeader = document.getElementById("world-header");
+const territoryHeader = document.getElementById("territory-header");
+const cityHeader = document.getElementById("city-header");
+const championHeader = document.getElementById("champion-header");
+const silverwingHeader = document.getElementById("silverwing-header");
 
-const worldHeader = document.getElementById('environment-header');
-const territoryHeader = document.getElementById('territory-header');
-const cityHeader = document.getElementById('city-header');
-const championHeader = document.getElementById('champion-header');
-const silverwingHeader = document.getElementById('silverwing-header');
+const environmentWorld = document.getElementById("environment-world");
+const environmentTerritory = document.getElementById("environment-territory");
+const environmentCity = document.getElementById("environment-city");
+const environmentChampion = document.getElementById("environment-champion");
+const environmentSilverwing = document.getElementById("environment-silverwing");
 
-const environmentWorld = document.getElementById('environment-world');
-const environmentTerritory = document.getElementById('environment-territory');
-const environmentCity = document.getElementById('environment-city');
-const environmentChampion = document.getElementById('environment-chmapion');
-const environmentSilverwing = document.getElementById('environment-silverwing');
+const worldSelect = document.getElementById("world-select");
+const territorySelect = document.getElementById("territory-select");
+const citySelect = document.getElementById("city-select");
+const championSelect = document.getElementById("champion-select");
+const silverwingSelect = document.getElementById("silverwing-select");
 
-
-
-
-
-const worldSelect = document.getElementById('world-select'); 
-const territorySelect = document.getElementById('territory-select');
-const citySelect = document.getElementById('city-select');
-const championSelect = document.getElementById('champion-select');
-const silverwingSelect = document.getElementById('silverwing-select');
-
-
-const worldImage = document.getElementById('enviroment-world');
-const territoryImage = document.getElementById('enviroment-territory');
-const cityImage = document.getElementById('enviroment-city');
-const championImage = document.getElementById('enviroment-champion');
-const silverwingImage = document.getElementById('enviroment-silverwing');
-
-
-
+const worldImage = document.getElementById("enviroment-world");
+const territoryImage = document.getElementById("enviroment-territory");
+const cityImage = document.getElementById("enviroment-city");
+const championImage = document.getElementById("enviroment-champion");
+const silverwingImage = document.getElementById("enviroment-silverwing");
 
 let environment = {
-        world: '',
-        territory: '',
-        city: '',
-        champion: '',
-        silverwing: '',
+  world: "",
+  territory: "",
+  city: "",
+  champion: "",
+  silverwing: "",
 };
 
-
-
-worldSelect.addEventListener('change', () => {
-environment.world = worldSelect.value;
-displayEnvironment();
-
-
+worldSelect.addEventListener("change", () => {
+  environment.world = worldSelect.value;
+  displayEnvironment();
 });
 
-territorySelect.addEventListener('change', () => {
-    environment.territory = territorySelect.value;
-    displayEnvironment();
-    
-    
-    });
+territorySelect.addEventListener("change", () => {
+  environment.territory = territorySelect.value;
+  displayEnvironment();
+});
 
-citySelect.addEventListener('change', () => {
-        environment.city = citySelect.value;
-        displayEnvironment();
-        
-        
-        });
-       
- championSelect.addEventListener('change', () => {
-            environment.champion = championSelect.value;
-            displayEnvironment();
-            
-            
-            });
-            silverwingSelect.addEventListener('change', () => {
-                environment.silverwing = silverwingSelect.value;
-                displayEnvironment();
-                
-                
-                }); 
+citySelect.addEventListener("change", () => {
+  environment.city = citySelect.value;
+  displayEnvironment();
+});
+
+championSelect.addEventListener("change", () => {
+  environment.champion = championSelect.value;
+  displayEnvironment();
+});
+silverwingSelect.addEventListener("change", () => {
+  environment.silverwing = silverwingSelect.value;
+  displayEnvironment();
+});
 
 //
 function displayEnvironment() {
-    worldHeader.textContent = environment.world;
-    environmentWorld.src = 'assets/' + environment.world + '.png';
+  worldHeader.textContent = environment.world;
+  environmentWorld.src = "assets/" + environment.world + ".png";
 
-    environmentTerritory.src = 'assets/' + environment.territory + '.png';
+  environmentTerritory.src = "assets/" + environment.territory + ".png";
 
-    environmentCity.src = 'assets/' + environment.city + '.png';
+  environmentCity.src = "assets/" + environment.city + ".png";
 
-    environmentChampion.src = 'assets/' + environment.champion + '.png';
+  environmentChampion.src = "assets/" + environment.champion + ".png";
 
-    environment.src = 'assets/' + environment.silverwing + '.png';
-
+  environment.src = "assets/" + environment.silverwing + ".png";
 }
 //const addButton = document.getElementById('add-button');
 displayEnvironment();
-;
